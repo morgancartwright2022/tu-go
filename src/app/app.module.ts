d01bd4c390c3cf7212cb8ee2338b61dfc7c6f81c
MorganCartwright2022 <morganjcartwright@gmail.com>
2022-04-24 12:46:35 -0500

    Added Bootstrap demo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HeadBannerComponent } from './head-banner/head-banner.component';
+import { BootstrapTestComponent } from './bootstrap-test/bootstrap-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeadBannerComponent
+    HeadBannerComponent,
+    BootstrapTestComponent
   ],
   imports: [
     BrowserModule,
